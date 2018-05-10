45437061bd154d087531868ca877d26f79abb79a
joncrall <crallj@rpi.edu>
2018-05-10 18:20:14 -0400

    Fixed python2 issue

--- mkinit/__init__.py
+++ mkinit/__init__.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 """
 Regenerate Input Command
-mkinit mkinit
+mkinit ~/code/mkinit/mkinit
 """
 # flake8: noqa
 __version__ = '0.0.2'
@@ -12,6 +13,9 @@ else:
     # <AUTOGEN_INIT>
     from mkinit import dynamic_mkinit
     from mkinit import static_mkinit
+    from mkinit import top_level_ast
     from mkinit.dynamic_mkinit import (dynamic_init,)
-    from mkinit.static_mkinit import (autogen_init,)
+    from mkinit.static_mkinit import (autogen_init, parse_submodule_definition,
+                                      static_init,)
+    from mkinit.top_level_ast import (TopLevelVisitor,)
     # </AUTOGEN_INIT>
--- mkinit/top_level_ast.py
+++ mkinit/top_level_ast.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 # from collections import OrderedDict
+from __future__ import absolute_import, division, print_function, unicode_literals
 import ast
 import six
 
@@ -70,7 +72,7 @@ class TopLevelVisitor(ast.NodeVisitor):
     def visit_If(self, node):
         """
         Notes:
-            elif clauses don’t have a special representation in the AST, but
+            elif clauses don't have a special representation in the AST, but
             rather appear as extra If nodes within the orelse section of the
             previous one.
         """
