a5923c0fac3623e083ffbc2b499caa3d253e2d95
joncrall <crallj@rpi.edu>
2018-05-10 18:51:30 -0400

    Added oset as a utility

--- mkinit/top_level_ast.py
+++ mkinit/top_level_ast.py
@@ -3,6 +3,57 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import ast
 import six
+from mkinit.orderedset import OrderedSet as oset
+
+
+_UNHANDLED = None
+
+
+def static_truthiness(node):
+    """
+    Extracts static truthiness of a node if possible
+
+    Args:
+        node (ast.Node)
+
+    Returns:
+        bool or None: True or False if a node can be statically bound to a
+        truthy value, otherwise returns None.
+    """
+    if isinstance(node, ast.Str):
+        return bool(node.s)
+    elif isinstance(node, ast.Tuple):
+        return bool(node.elts)
+    elif isinstance(node, ast.Num):
+        return bool(node.n)
+    elif six.PY3 and isinstance(node, ast.Bytes):  # nocover
+        return bool(node.s)
+    elif six.PY3 and isinstance(node, ast.NameConstant):
+        return bool(node.value)
+    elif six.PY2 and isinstance(node, ast.Name):  # nocover
+        constants_lookup = {
+            'True': True,
+            'False': False,
+            'None': None,
+        }
+        try:
+            return bool(constants_lookup[node.id])
+        except KeyError:
+            return _UNHANDLED
+    else:
+        return _UNHANDLED
+
+
+def get_conditional_attrnames(nodes):
+    """
+    Gets attrnames within a list of nodes
+    """
+    sub_visitor = TopLevelVisitor()
+    for node in nodes:
+        # Check the attributes defined on this branch
+        sub_visitor.visit(node)
+    got_attrnames = sub_visitor.attrnames
+    return got_attrnames
 
 
 class TopLevelVisitor(ast.NodeVisitor):
@@ -12,6 +63,9 @@ class TopLevelVisitor(ast.NodeVisitor):
     References:
         # For other visit_<classname> values see
         http://greentreesnakes.readthedocs.io/en/latest/nodes.html
+
+    CommandLine:
+        python ~/code/mkinit/mkinit/top_level_ast.py TopLevelVisitor
 
     Example:
         >>> from xdoctest import utils
@@ -30,13 +84,48 @@ class TopLevelVisitor(ast.NodeVisitor):
         ...            pass
         ...    ''')
         >>> self = TopLevelVisitor.parse(source)
-        >>> attrnames = set(self.attrnames)
-        >>> assert attrnames == {'foo', 'bar', 'Spam'}
-        >>> assert 'subfunc' not in self.attrnames
+        >>> print('attrnames = {!r}'.format(sorted(self.attrnames)))
+        attrnames = ['Spam', 'bar', 'foo']
+
+    Example:
+        >>> from xdoctest import utils
+        >>> source = utils.codeblock(
+        ...    '''
+        ...    a = True
+        ...    if a:
+        ...        b = True
+        ...        c = True
+        ...    else:
+        ...        b = False
+        ...    ''')
+        >>> self = TopLevelVisitor.parse(source)
+        >>> print('attrnames = {!r}'.format(sorted(self.attrnames)))
+        attrnames = ['a', 'b']
+
+    Example:
+        >>> from xdoctest import utils
+        >>> source = utils.codeblock(
+        ...    '''
+        ...    try:
+        ...        d = True
+        ...        e = True
+        ...    except Exception:
+        ...        d = False
+        ...    ''')
+        >>> self = TopLevelVisitor.parse(source)
+        >>> print('attrnames = {!r}'.format(sorted(self.attrnames)))
     """
     def __init__(self):
         super(TopLevelVisitor, self).__init__()
-        self.attrnames = []
+        self.attrnames = oset()
+
+    def _register(self, name):
+        if isinstance(name, (list, tuple)):
+            for n in name:
+                self._register(n)
+        else:
+            if name not in self.attrnames:
+                self.attrnames.append(name)
 
     @classmethod
     def parse(TopLevelVisitor, source):
@@ -51,22 +140,16 @@ class TopLevelVisitor(ast.NodeVisitor):
     #     super(TopLevelVisitor, self).visit(node)
 
     def visit_FunctionDef(self, node):
-        callname = node.name
-        self.attrnames.append(callname)
+        self._register(node.name)
 
     def visit_ClassDef(self, node):
-        callname = node.name
-        self.attrnames.append(callname)
+        self._register(node.name)
 
     def visit_Assign(self, node):
-        # print('VISIT FunctionDef node = %r' % (node,))
-        # print('VISIT FunctionDef node = %r' % (node.__dict__,))
         for target in node.targets:
             if hasattr(target, 'id'):
-                self.attrnames.append(target.id)
-        # print('node.value = %r' % (node.value,))
-        # TODO: assign constants to
-        # self.const_lookup
+                self._register(target.id)
+        # TODO: assign constants to self.const_lookup?
         self.generic_visit(node)
 
     def visit_If(self, node):
@@ -88,66 +171,26 @@ class TopLevelVisitor(ast.NodeVisitor):
             except Exception:  # nocover
                 pass
 
-        unhandled = object()
-
-        def static_truthiness(item):
-            if isinstance(item, ast.Str):
-                return bool(item.s)
-            elif isinstance(item, ast.Tuple):
-                return bool(item.elts)
-            elif isinstance(item, ast.Num):
-                return bool(item.n)
-            elif six.PY2 and isinstance(item, ast.Bytes):  # nocover
-                return bool(item.s)
-            elif six.PY2 and isinstance(item, ast.Name):  # nocover
-                constants_lookup = {
-                    'True': True,
-                    'False': False,
-                    'None': None,
-                }
-                return constants_lookup.get(item.value, unhandled)
-            elif six.PY3 and isinstance(item, ast.NameConstant):
-                return item.value
-            else:
-                return unhandled
-
-        def get_conditional_attrnames(nodes):
-            # Save state
-            orig_callnames = self.attrnames
-            self.attrnames = []
-            for node in nodes:
-                # Check the attributes defined on this branch
-                self.visit(node)
-            got_attrnames = self.attrnames
-            # Restore state
-            self.attrnames = orig_callnames
-            return got_attrnames
-
         def check_condition_definitions(node, common=None):
             """
             Find definitions from conditionals that always accept or
             that are defined in all possible branches (note this requires an
             else statment)
             """
-            try:
-                import ubelt as ub
-                set_ = ub.oset
-            except ImportError:
-                set_ = set
             has_elseif = len(node.orelse) == 1 and isinstance(node.orelse[0], ast.If)
             has_else = node.orelse and not has_elseif
 
             # returns True if this branch will unconditionally accept, False,
             # if it will unconditionally reject, and None if it is uncertain.
             truth = static_truthiness(node.test)
-            if truth is not unhandled:
+            if truth is not _UNHANDLED:
                 if truth:
                     # No need to check other conditionals
                     attrnames = get_conditional_attrnames(node.body)
                     return attrnames
             else:
                 # Find the attrs in this branch and intersect them with common
-                attrnames = set_(get_conditional_attrnames(node.body))
+                attrnames = oset(get_conditional_attrnames(node.body))
                 if common is None:
                     common = attrnames
                 else:
@@ -156,7 +199,7 @@ class TopLevelVisitor(ast.NodeVisitor):
             if has_else:
                 # If we get to an else, return the common attributes between
                 # all non-rejecting branches
-                else_attrs = set_(get_conditional_attrnames(node.orelse))
+                else_attrs = oset(get_conditional_attrnames(node.orelse))
                 if common is None:
                     common = else_attrs
                 else:
@@ -171,7 +214,20 @@ class TopLevelVisitor(ast.NodeVisitor):
         # FIXME: will not handled deleted attributes
         common = check_condition_definitions(node)
         if common:
-            self.attrnames.extend(list(common))
+            self._register(list(common))
+
+    def visit_TryExcept(self, node):
+        """
+        We only care about checking if (a) a variable is defined in the main
+        body, and (b) that the variable is defined in all except blacks that
+        **don't** immediately re-raise.
+        """
+        # TODO
+        self.generic_visit(node)
+
+    def visit_Del(self, node):
+        # TODO
+        self.generic_visit(node)
 
     # def visit_ExceptHandler(self, node):
     #     pass
@@ -179,10 +235,16 @@ class TopLevelVisitor(ast.NodeVisitor):
     # def visit_TryFinally(self, node):
     #     pass
 
-    # def visit_TryExcept(self, node):
-    #     pass
-
     # def visit_Try(self, node):
     #     TODO: parse a node only if it is visible in all cases
     #     pass
     #     # self.generic_visit(node)  # nocover
+
+
+if __name__ == '__main__':
+    """
+    CommandLine:
+        python -m mkinit.top_level_ast all
+    """
+    import xdoctest
+    xdoctest.doctest_module(__file__)
