95165df31d8dea993f5027e3364a1e778edf3c20
Jon Crall <jon.crall@kitware.com>
2018-07-13 16:55:31 -0400

    wip

--- tests/mkinit_dummy_module/__init__.py
+++ tests/mkinit_dummy_module/__init__.py
@@ -1,2 +1,9 @@
+from mkinit_dummy_module import subdir1
 from mkinit_dummy_module import submod1
 from mkinit_dummy_module import submod2
+
+from mkinit_dummy_module.submod1 import (func1, func2,)
+from mkinit_dummy_module.submod2 import (class1, class2,)
+
+__all__ = ['class1', 'class2', 'func1', 'func2', 'subdir1', 'submod1',
+           'submod2']
---
+++ tests/mkinit_dummy_module/submod2.py
@@ -0,0 +1,6 @@
+class class1:
+    pass
+
+
+class class2:
+    pass
