6c96596de9cfe5c4931dc5f521b5f4647288f9e8
joncrall <erotemic@gmail.com>
2021-02-14 15:40:16 -0500

    Simplify block

--- mkinit/formatting.py
+++ mkinit/formatting.py
@@ -281,18 +281,18 @@ def _initstr(
         >>> initstr = _initstr(modname, imports, from_imports, options=options)
         >>> print(initstr.replace('\n\n', '\n'))
         from importlib import lazy_import
-          __getattr__ = lazy_import(
-              __name__,
-              submodules={
-                  'bar',
-                  'baz',
-              },
-              submod_attrs={
-                  'bar': [
-                      'func1',
-                      'func2',
-                  ],
-              },
+        __getattr__ = lazy_import(
+            __name__,
+            submodules=[
+                'bar',
+                'baz',
+            ],
+            submod_attrs={
+                'bar': [
+                    'func1',
+                    'func2',
+                ],
+            },
         )
         def __dir__():
             return __all__
@@ -391,26 +391,12 @@ def _initstr(
                     for func in funcs
                 }
 
-                def require(fullname):
-                    if fullname in sys.modules:
-                        return sys.modules[fullname]
-                    spec = importlib.util.find_spec(fullname)
-                    try:
-                        module = importlib.util.module_from_spec(spec)
-                    except Exception:
-                        raise ImportError(
-                            'Could not lazy import module {fullname}'.format(
-                                fullname=fullname)) #  from None
-                    loader = importlib.util.LazyLoader(spec.loader)
-                    sys.modules[fullname] = module
-                    loader.exec_module(module)
-                    return module
-
                 def __getattr__(name):
                     if name in submodules:
-                        fullname = '{module_name}.{name}'.format(
-                            module_name=module_name, name=name)
-                        attr = require(fullname)
+                        attr = importlib.import_module(
+                            '{module_name}.{modname}'.format(
+                                module_name=module_name, modname=modname)
+                        )
                     elif name in name_to_submod:
                         modname = name_to_submod[name]
                         module = importlib.import_module(
