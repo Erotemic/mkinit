17885918a6af99d75c200675f77c2ddddc40378d
joncrall <erotemic@gmail.com>
2022-12-03 14:49:49 -0500

    Support the lazy_loader module

--- demo/mkinit_demo_pkg/submod.py
+++ demo/mkinit_demo_pkg/submod.py
@@ -1,4 +1,7 @@
 # --- submod.py ---
 
 def submod_func():
-    print('This is a submod func in {}'.format(__file__))
+    print('You have called the submod func in {}'.format(__file__))
+
+
+print('The submod import has been executed')
--- demo/mkinit_demo_pkg/subpkg/nested.py
+++ demo/mkinit_demo_pkg/subpkg/nested.py
@@ -1,2 +1,4 @@
 def nested_func():
-    print('This is a nested func in {}'.format(__file__))
+    print('You have called the nested func in {}'.format(__file__))
+
+print('The subpkg.nested import has been executed')
